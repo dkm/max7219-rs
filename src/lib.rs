28ade4d344726b831cce706612365eb6335c67da
Marc Poulhiès <dkm@kataplop.net>
2018-04-13 13:51:29 +0200

    support for naive pixel buffers

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,58 @@ enum Max7219Regs {
     DisplayTest = 0xf,
 }
 
+/// Nada
+pub struct PixArray {
+    pixels : [u8;8]
+}
+
+impl PixArray {
+
+    /// Creates new pix
+    pub fn new() -> PixArray {
+        PixArray { pixels: [0; 8] }
+    }
+
+    /// From char
+    pub fn from(c : char) -> PixArray {
+        match c {
+            'a' => PixArray { pixels : [0b00000000,
+                                        0b00011000,
+                                        0b00100100,
+                                        0b01111110,
+                                        0b10000001,
+                                        0b00000000,
+                                        0b00000000,
+                                        0b00000000]},
+            _ => PixArray { pixels: [0;8] }
+        }
+    }
+
+    /// set pixel
+    pub fn set_pixel(&mut self, line : usize, col : usize , v : bool) {
+        if v {
+            self.pixels[line] |= 1<<col;
+        } else {
+            self.pixels[line] &= !(1<<col );
+        }
+    }
+
+    /// get pixel
+    pub fn get_pixel(&self, line : usize, col : usize) -> u8 {
+        self.pixels[line] & (1<<col) as u8
+    }
+
+    /// Get line
+    pub fn get_pixel_line(&self, l : usize) -> u8 {
+        self.pixels[l]
+    }
+
+    /// Set line
+    pub fn set_pixel_line(&mut self, l : usize, v: u8) {
+        self.pixels[l] = v;
+    }
+}
+
 impl From<u8> for Max7219Regs {
     fn from(reg_index: u8) -> Self {
         match reg_index {
@@ -50,6 +102,7 @@ pub struct Max7219<S,P> {
     /// bob
     spi : S,
     cs : P,
+    num: u8
 }
 
 impl<S,P> Max7219<S,P>
@@ -57,10 +110,11 @@ where S: hal::spi::FullDuplex<u8>,
       P: hal::digital::OutputPin {
 
     /// Creates a new device descriptor
-    pub fn new(spi: S, cs: P) -> Max7219<S,P> {
+    pub fn new(spi: S, cs: P, num: u8) -> Max7219<S,P> {
         Max7219 {
             spi : spi,
             cs : cs,
+            num : num
         }
     }
 
@@ -82,30 +136,44 @@ where S: hal::spi::FullDuplex<u8>,
     }
 
     /// Writes a line
-    pub fn write_line(&mut self, line: u8, val: u8) {
-        self.set_reg(Max7219Regs::from(line), val);
+    pub fn write_lines(&mut self, line_index: u8, vals: &[u8]) {
+        for i in 0..self.num {
+            self.set_reg(Max7219Regs::from(line_index), vals[i as usize]);
+        }
+    }
+
+    /// Writes a pixel buffer
+    pub fn write_pixbufs(&mut self, pixbufs: &[PixArray]) {
+        for l in 0..8 {
+            let line = Max7219Regs::from(l);
+            for i in (0..self.num).rev() {
+                self.set_reg(line, pixbufs[i as usize].get_pixel_line(l as usize));
+            }
+        }
     }
 
     /// Initializes the device
     pub fn init(&mut self) {
-        self.cs.set_high();
+        for _i in 0..self.num {
+            self.cs.set_high();
 
-        // Shutdown
-        self.set_reg(Max7219Regs::Shutdown, 0);
+            // Shutdown
+            self.set_reg(Max7219Regs::Shutdown, 0);
 
-        // Midpower intensity
-        self.set_reg(Max7219Regs::Intensity, 0x4);
+            // Midpower intensity
+            self.set_reg(Max7219Regs::Intensity, 0x4);
 
-        // Disable test mode
-        self.set_reg(Max7219Regs::DisplayTest, 0x0);
+            // Disable test mode
+            self.set_reg(Max7219Regs::DisplayTest, 0x0);
 
-        // Disable char decoding
-        self.set_reg(Max7219Regs::DecodeMode, 0);
+            // Disable char decoding
+            self.set_reg(Max7219Regs::DecodeMode, 0);
 
-        // Display all digit
-        self.set_reg(Max7219Regs::ScanLimit, 0x07);
+            // Display all digit
+            self.set_reg(Max7219Regs::ScanLimit, 0x07);
 
-        // Enable
-        self.set_reg(Max7219Regs::Shutdown, 1);
+            // Enable
+            self.set_reg(Max7219Regs::Shutdown, 1);
+        }
     }
 }
